dc2a8e243617f5c41f4d056f52ba88a661956e2f
MukeshG7172 <mukeshrjpm1@gmail.com>
2026-01-06 13:42:04 +0530

    feat: Next update

--- src/app/api/individual-task/route.js
+++ src/app/api/individual-task/route.js
@@ -1,6 +1,6 @@
 // src/app/api/individual-task/route.ts
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '@/lib/prisma'
+import prisma from '@/lib/prisma'
 
 export async function POST(request) {
   try {
